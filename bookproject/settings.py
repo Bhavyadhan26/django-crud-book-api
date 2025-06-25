7030a009c7aafbade54aab3bfe47896b4e943c35
Bhavya Dhanuka <kesudhanuka@gmail.com>
2025-06-25 17:01:17 +0530

    Updated .env file

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x!c#2l^(cdz6q$(cb4z#_p^*(tt5i31(ehc$&qgzdywhg&rb%0'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
